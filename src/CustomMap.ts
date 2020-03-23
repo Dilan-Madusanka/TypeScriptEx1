356b4c9bb43650b5d21f54e34928d95eb66d3fb7
unknown <dilanmax93@gmail.com>
2020-03-23 21:10:12 +0530

    Initial Commit

---
+++ src/CustomMap.ts
@@ -0,0 +1,39 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1, //7.7
+      center: {
+        lat: 0, //8
+        lng: 0 //80.5
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
